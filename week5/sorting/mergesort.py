f9826ab7456af1fce8591840050a29883f4fdba6
solideveloper <stephanie.olivares@bryanuniversity.edu>
2021-12-31 12:51:35 -0500

    sorting

---
+++ week5/sorting/mergesort.py
@@ -0,0 +1,36 @@
+def mergeSort(nlist):
+    print("Splitting ",nlist)
+    if len(nlist)>1:
+        mid = len(nlist)//2
+        lefthalf = nlist[:mid]
+        righthalf = nlist[mid:]
+        mergeSort(lefthalf)
+        mergeSort(righthalf)
+        nlist = merge(nlist,lefthalf,righthalf)
+    print("Merging ",nlist)
+
+def merge(nlist,lefthalf,righthalf):
+    i=j=k=0       
+    while i < len(lefthalf) and j < len(righthalf):
+        if lefthalf[i] < righthalf[j]:
+            nlist[k]=lefthalf[i]
+            i=i+1
+        else:
+            nlist[k]=righthalf[j]
+            j=j+1
+        k=k+1
+
+    while i < len(lefthalf):
+        nlist[k]=lefthalf[i]
+        i=i+1
+        k=k+1
+
+    while j < len(righthalf):
+        nlist[k]=righthalf[j]
+        j=j+1
+        k=k+1
+    return nlist
+
+data = [55, 31, 26, 20, 63, 7, 51, 74, 81, 40]
+mergeSort(data)
+print("Sorted:", data)
